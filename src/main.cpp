0b9923e32d3dcc6c1a381e8f57cb2f9782eb9a6b
TheKalata <kaloyan.zh.atanasov.2020@elsys-bg.org>
2024-07-04 10:13:19 +0300

    configured loading with glad

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,6 @@
+#include "glad.h"
 #include <GLFW/glfw3.h>
+#include <iostream>
 
 int main(void)
 {
@@ -9,7 +11,7 @@ int main(void)
         return -1;
 
     /* Create a windowed mode window and its OpenGL context */
-    window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+    window = glfwCreateWindow(640, 480, "Left indicator", NULL, NULL);
     if (!window)
     {
         glfwTerminate();
@@ -19,11 +21,44 @@ int main(void)
     /* Make the window's context current */
     glfwMakeContextCurrent(window);
 
+    if(!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) 
+    {
+        glfwTerminate();
+        return -1;
+    }
+
     /* Loop until the user closes the window */
     while (!glfwWindowShouldClose(window))
     {
         /* Render here */
         glClear(GL_COLOR_BUFFER_BIT);
+
+        glColor3f(0.f, 1.f, 0.f);
+
+        // hollow indicator
+        glLineWidth(10.f);
+        glBegin(GL_LINE_LOOP);
+        glVertex2f(-0.5f, 0.0f);
+        glVertex2f(0.0f, 0.5f);
+        glVertex2f(0.0f, 0.25f);
+        glVertex2f(0.5f, 0.25f);
+        glVertex2f(0.5f, -0.25f);
+        glVertex2f(0.0f, -0.25f);
+        glVertex2f(0.0f, -0.5f);
+
+        /* normal indicator
+        glBegin(GL_TRIANGLES);
+        glVertex2f(-0.5f, 0.0f); //part 1
+        glVertex2f(0.0f, 0.5f);  //part 1
+        glVertex2f(0.0f, -0.5f); //part 1
+        glVertex2f(0.0f, 0.25f); //part 2.1
+        glVertex2f(0.0f, -0.25f);//part 2.1
+        glVertex2f(0.5f, 0.25f); //part 2.1
+        glVertex2f(0.5f, -0.25f);//part 2.2
+        glVertex2f(0.5f, 0.25f); //part 2.2
+        glVertex2f(0.0f, -0.25f);//part 2.2*/
+
+        glEnd();
 
         /* Swap front and back buffers */
         glfwSwapBuffers(window);
